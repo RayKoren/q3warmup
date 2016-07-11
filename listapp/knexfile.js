efb870d55170a0ebcf308e097282af2d74a107d7
rayk216 <raykoren@gmail.com>
2016-07-11 09:41:45 -0600

    added db

---
+++ listapp/knexfile.js
@@ -0,0 +1,13 @@
+// Update with your config settings.
+require('dotenv').load();
+module.exports = {
+
+    development: {
+        client: 'pg',
+        connection: 'postgres://localhost/listapp'
+    },
+    production: {
+        client: 'pg',
+        connection: process.env.DATABASE_URL + '?ssl=true'
+    }
+};
